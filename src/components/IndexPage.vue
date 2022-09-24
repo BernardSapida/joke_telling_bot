b46021b726e5e65f711f51867d53b28e9e9de00b
BernardSapida <bernardsapida1706@gmail.com>
2022-09-24 23:17:21 +0800

    commit

--- src/components/IndexPage.vue
+++ src/components/IndexPage.vue
@@ -10,13 +10,15 @@
         <div class="qoobee_joke mb-3 pt-3 pb-1 px-3 rounded">
           <p>{{maleMessage}}</p>
         </div>
-        <img src="../../public/images/QoobeeThinking.gif" height="250px" alt="Qoobee Thinking">
+        <img v-if="isMaleThinking" src="../../public/images/QoobeeThinking.gif" height="250px" alt="Qoobee Thinking">
+        <img v-else src="../../public/images/QoobeeJoking.gif" height="250px" alt="Qoobee Joking">
       </div>
       <div class="col-6 m-auto">
         <div class="qoobee_question mb-5 pt-3 pb-2 px-3 rounded">
           <p>{{femaleMessage}}</p>
         </div>
-        <img src="../../public/images/QoobeeAsking.gif" height="210px" alt="Qoobee Asking">
+        <img v-if="isFemaleAsking" src="../../public/images/QoobeeAsking.gif" height="210px" alt="Qoobee Asking">
+        <img v-else src="../../public/images/QoobeeLaughing.gif" height="210px" alt="Qoobee Laughing">
         <button v-if="speechEnded" class="btn d-block mt-3 m-auto askQuestion px-3 py-2" @click="talkQoobee('Love, can you please make me laugh?', 'female')">TELL ME A JOKE</button>
       </div>
     </div>
@@ -28,12 +30,15 @@ export default {
   name: 'IndexPage',
   data() {
     return {
+      isMaleThinking: true,
+      isFemaleAsking: true,
+      femaleState: 'QoobeeAsking.gif',
       femaleMessage: "Hello love! How are you?",
       maleMessage: "I'm fine, thanks!",
       synth: window.speechSynthesis,
       voiceList: [],
       choosenVoice: 0,
-      speechEnded: true
+      speechEnded: false
     }
   },
   mounted() {
@@ -41,9 +46,11 @@ export default {
     this.synth.onvoiceschanged = () => {
       this.voiceList = this.synth.getVoices();
       if(this.speechEnded == false) {
-        // this.talkQoobee(this.femaleMessage, "female");
-        // this.talkQoobee(this.maleMessage, "male");
-        // setTimeout(() => this.speechEnded = true, 6000)
+        setTimeout(() => {
+          this.talkQoobee(this.femaleMessage, "female");
+          this.talkQoobee(this.maleMessage, "male");
+          this.speechEnded = true
+        }, 2000)
       }
     }
   },
@@ -61,17 +68,23 @@ export default {
 
       if(grammar == 'Love, can you please make me laugh?') {
         this.talkQoobee("Yes, sure HAHA!", "male")
-        setTimeout(() => this.speechEnded = true, 6000)
+        this.isMaleThinking = true;
+        this.isFemaleAsking = true;
         setTimeout(() => this.randomJokes(), 6000);
       }
     },
     async randomJokes() {
       let requestJoke = await fetch("https://official-joke-api.appspot.com/jokes/programming/random");
       let response = await requestJoke.json();
-      this.talkQoobee(response[0].setup, "male");
+      console.log(response[0])
+      this.talkQoobee(response[0].setup, "male")
+      this.isMaleThinking = false;
       await setTimeout(() => this.talkQoobee(response[0].punchline, "male"), 6000);
-      await setTimeout(() => this.talkQoobee("You are funny! I love you, love!", "female"), 6000);
-      this.speechEnded = true;
+      await setTimeout(() => { 
+        this.talkQoobee("You are funny haha! I love you, love!", "female");
+        this.isFemaleAsking = false;
+      }, 6000);
+      await setTimeout(() => this.speechEnded = true, 6000);
     }
   }
 }
