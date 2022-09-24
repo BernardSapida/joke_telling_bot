c8e6076b60b5d10f3b0d2d3dac5fad6c87025516
BernardSapida <bernardsapida1706@gmail.com>
2022-09-24 16:52:02 +0800

    commit

---
+++ src/components/IndexPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-auto m-auto">
+        <img src="../../public/images/QoobeeThinking.gif" height="250px" alt="Qoobee Thinking">
+      </div>
+      <div class="col-auto m-auto">
+        <img src="../../public/images/QoobeeAsking.gif" height="220px" alt="Qoobee Asking">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
